14f1a2a2e2174327efe0887d559dcb0bece52b9a
James McCay <jwmccay@pm.me>
2025-01-20 20:46:01 -0800

    Argparse for run combined

--- scripts/run_combined.py
+++ scripts/run_combined.py
@@ -2,37 +2,78 @@
 Combined calc
 """
 
+import argparse
+
 from brisbane_sunset.containers import Origin, Date
 from brisbane_sunset.dusk import (standard_preparation,
                                   interp_wrapper,
                                   time_blocked)
 
 
+def parse_date(date_string: str) -> Date:
+    """"Turns YYYY-M(M)-D(D) string into Date object"""
+
+    date_list = date_string.split("-")
+    date_list = [int(d) for d in date_list]
+
+    assert len(date_list) == 3, f"{date_string} cannot be parsed"
+
+    date = Date(date_list[0], date_list[1], date_list[2])
+
+    return date
+
+
+def parse_coordinate(coord_string: str):
+    """"Turns Lat,Lon into a two element list of floats
+
+    The first element is the latitude and the second is the
+    longitude. Both must be in decimal format.
+    """
+
+    coord_list = coord_string.split(",")
+    coord_list = [float(c) for c in coord_list]
+
+    assert len(coord_list) == 2, "too many or two few coordinate dimensions"
+
+    return coord_list
+
+
 if __name__ == "__main__":
 
-    year = 2025
-    month = 1
-    day = 18
+    parser = argparse.ArgumentParser(
+                        prog='sunset-run-combined',
+                        description='Reproject an SRTM tif')
+
+    parser.add_argument("-r", "--raster")
+    parser.add_argument("-d", "--date")
+    parser.add_argument("-oc", "--origin_coordinate")
+    parser.add_argument("-cm", "--coord_mode", default="xy")
+    parser.add_argument("-e", "--epsg", default=7131, type=int)
+    parser.add_argument("-dp", "--draw_plots", action='store_true')
+    parser.add_argument("-fd", "--figure_directory", default=None)
+
+    args = parser.parse_args()
+
+    date = parse_date(args.date)
+    lat_origin, lon_origin = parse_coordinate(args.origin_coordinate)
+    raster_fname = args.raster
+    coord_mode = args.coord_mode
+    draw_plots = args.draw_plots
+    figure_directory = args.figure_directory
+
+    if coord_mode == "xy":
+        epsg_latlon = 4326
+        epsg_xy = args.epsg
+    elif coord_mode == "latlon":
+        epsg_latlon = None
+        epsg_xy = None
+    else:
+        assert False, f"coord_mode {coord_mode} is not a valid coordinate mode"
+
+    # Options that have not migrated to argparse yet
     distance = 1800.0
-
-    # Mission Blue
-    lon_origin = -122.4150331485603
-    lat_origin = 37.692434406915844
-
-    draw_plots = True
-
     # interp_mode = "RegularGrid"
     interp_mode = "LinearND"
-
-    # coord_mode = "latlon"
-    # raster_fname = "data/n37_w123_subset.tif"
-    # epsg_latlon = None
-    # epsg_xy = None
-
-    raster_fname = "data/n37_w123_subset_reproject.tif"
-    coord_mode = "xy"
-    epsg_latlon = 4326
-    epsg_xy = 7131
 
     rd, interp, transformer = standard_preparation(
         raster_fname,
@@ -51,13 +92,12 @@ if __name__ == "__main__":
                         interp, x_origin, y_origin))
     origin.init_xy(x_origin, y_origin)
 
-    date = Date(year, month, day)
-
     dt = time_blocked(origin, distance, date, interp, rd,
-                      draw_plots=draw_plots,
+                      draw_plots=args.draw_plots,
                       interp_mode=interp_mode,
                       coord_mode=coord_mode,
-                      num_points=10000)
+                      num_points=10000,
+                      fig_dir=figure_directory)
 
     hour = dt.hour - 12
     minute = str(dt.minute).rjust(2, "0")
