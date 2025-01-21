33fbbbad1445234cb49b869d1ee24f034a775877
James McCay <jwmccay@pm.me>
2025-01-20 20:03:05 -0800

    Move reproject script to entry point

--- src/brisbane_sunset/__init__.py
+++ src/brisbane_sunset/__init__.py
@@ -1,8 +1,10 @@
 from brisbane_sunset.scripts.view_tif import view_tif
 from brisbane_sunset.scripts.subset_tif import subset_tif
+from brisbane_sunset.scripts.reproject_tif import reproject_tif
 
 
 __all__ = [
     "view_tif",
-    "subset_tif"
+    "subset_tif",
+    "reproject_tif"
 ]
