62116e4de9c6d5c99f08850861e32d021145bb00
James McCay <jwmccay@pm.me>
2025-03-03 21:32:35 -0800

    Kinda works but flipped

--- scripts_dev/build_raster.py
+++ scripts_dev/build_raster.py
@@ -11,19 +11,28 @@ deg_lon = -122.0
 deg_lat = 38.0
 delta_lon = 1.0
 delta_lat = 1.0
+n_lat = 240
+n_lon = 180
 
-x = np.linspace(-4.0, 4.0, 240)
-y = np.linspace(-3.0, 3.0, 180)
+# Kinda works but x and y are flipped
+
+x = np.linspace(deg_lat, deg_lat + delta_lat, n_lat)
+y = np.linspace(deg_lon, deg_lon + delta_lon, n_lon)
 X, Y = np.meshgrid(x, y)
-Z1 = np.exp(-2 * np.log(2) * ((X - 0.5) ** 2 + (Y - 0.5) ** 2) / 1 ** 2)
-Z2 = np.exp(-3 * np.log(2) * ((X + 0.5) ** 2 + (Y + 0.5) ** 2) / 2.5 ** 2)
+
+# Build a Z grid on something that's the same size
+x_temp = np.linspace(-4.0, 4.0, n_lat)
+y_temp = np.linspace(-3.0, 3.0, n_lon)
+X_temp, Y_temp = np.meshgrid(x_temp, y_temp)
+Z1 = np.exp(-2 * np.log(2)
+            * ((X_temp - 0.5) ** 2 + (Y_temp - 0.5) ** 2) / 1 ** 2)
+Z2 = np.exp(-3 * np.log(2)
+            * ((X_temp + 0.5) ** 2 + (Y_temp + 0.5) ** 2) / 2.5 ** 2)
 Z = 10.0 * (Z2 - Z1)
 
-res = (x[-1] - x[0]) / 240.0
-transform = Affine.translation(x[0] - res / 2, y[0] - res / 2) * Affine.scale(res, res)
-transform
-Affine(0.033333333333333333, 0.0, -4.0166666666666666,
-       0.0, 0.033333333333333333, -3.0166666666666666)
+res = (x[-1] - x[0]) / n_lat
+transform = Affine.translation(
+    x[0] - res / 2, y[0] - res / 2) * Affine.scale(res, res)
 
 new_dataset = rasterio.open(
      './new.tif',
