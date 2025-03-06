8291e8dbb949dddf53bbe8e715d5a82e1daa5703
James McCay <jwmccay@pm.me>
2025-03-05 21:38:45 -0800

    Informal test to support development

---
+++ informal_test.sh
@@ -0,0 +1,23 @@
+mkdir data_test
+cp data/n37_w123_1arc_v3.tif data_test
+
+echo "Subset..."
+sunset-subset-tif \
+    -i "data_test/n37_w123_1arc_v3.tif" \
+    -o "data_test/n37_w123_subset.tif" \
+    -b 1900 900 400 400
+
+echo "Reproject..."
+sunset-reproject-tif \
+    -i data_test/n37_w123_subset.tif \
+    -o data_test/n37_w123_subset_reproject.tif \
+    -e 7131
+
+echo "Run..."
+echo "Expect approx: \"Sunset at 4:09 on 1/18/2025\""
+sunset-run -d "2025-1-18" -oc "37.6924344,-122.4150331" \
+    -r "data_test/n37_w123_subset_reproject.tif" \
+    -cm "xy" \
+    -dp -fd "data_test/figs"
+
+rm -r data_test
